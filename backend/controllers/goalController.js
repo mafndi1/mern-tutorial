77251b0615ed60be6836071f6cf1ad361064e344
mafndi1 <191240000927@unisnu.ac.id>
2022-07-15 21:23:41 +0700

    Accepting Body Data

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -8,6 +8,10 @@ const getGoals = (req,res) => {
 // @route   POST /api/goals
 // @access  Private
 const setGoal = (req,res) => {
+    if(!req.body.text){
+        res.status(400).json({message :'Please add a text field'})
+        }
+
     res.status(200).json({message: 'Set Goals'})
 }
 // @desc    Update Goals
