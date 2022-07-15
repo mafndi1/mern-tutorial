21e3aaa50bdf26076406f51ec2786c7f68af3e89
mafndi1 <191240000927@unisnu.ac.id>
2022-07-15 21:01:29 +0700

    First Route

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
-console.log("Hello World")
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 
 const app = express()
 
-app.listen(port, ()=> console.log(`Server started on port ${port}`)) 
+app.get('/api/goals',(req,res)=>{
+    res.status(200).json({message: 'Get Goals'})
+})
