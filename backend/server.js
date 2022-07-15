0bcab57e221237bb4da9fa58634574d3377fbb5a
mafndi1 <191240000927@unisnu.ac.id>
2022-07-15 21:41:01 +0700

    Connect with Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,11 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
-const {errorHandler} = require('../middleware/errorMiddleware')
+const connectDb = require('./config/db')
+const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
+connectDb()
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({extended : false}))
-
-app.get('/api/goals',(req,res)=>{
-    res.status(200).json({message: 'Get Goals'})
-})
-
-app.use('/api/goals',require('./routes/goalRoutes.js'))
-
-app.use(errorHandler)
-app.listen(port, ()=> console.log(`Server started on port ${port}`)) 
