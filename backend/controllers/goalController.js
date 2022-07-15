875bd9de47ee55a43f0f8d44f98d14ab65df65d6
mafndi1 <191240000927@unisnu.ac.id>
2022-07-15 22:10:07 +0700

    Merge branch 'master' of https://github.com/mafndi1/mern-tutorial

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -5,38 +5,33 @@ const Goal = require('../models/goalModel')
 // @desc    Get Goals
 // @route   GET /api/goals
 // @access  Private
-const getGoals = (req,res) => {
 const getGoals = asyncHandler(async(req,res) => {
+    res.status(200).json({message: 'Get Goals'})
     const goals = await Goal.find()
     res.status(200).json(goals)
-}
 })
 // @desc    Set Goals
 // @route   POST /api/goals
-// @access  Private
-const setGoal = (req,res) => {
-const setGoal = asyncHandler(async(req,res) => {
-    if(!req.body.text){
-        res.status(400)
+@@ -16,20 +18,44 @@ const setGoal = asyncHandler(async(req,res) => {
          throw new Error('Please add a text field')
         }
 
-        
         const goal = await Goal.create({
             text: req.body.text,
 
 
         })
 
-        res.status(200).json(goal)
-}
+    res.status(200).json({message: 'Set Goals'})
+
+    res.status(200).json(goal)
 })
 // @desc    Update Goals
 // @route   GET /api/goals:id
 // @route   PUT /api/goals:id
 // @access  Private
-const updateGoal = (req,res) => {
 const updateGoal = asyncHandler(async(req,res) => {
+    res.status(200).json({message: 'Update Goals ${req.params.id}'})
     const goal = await Goal.findById(req.params.id)
     if(!goal) {
         res.status(400)
@@ -47,13 +42,12 @@ const updateGoal = asyncHandler(async(req,res) => {
 
 
     res.status(200).json(updateGoal)
-}
 })
 // @desc    Delete Goals
 // @route   GET /api/goals:id
 // @access  Private
-const deleteGoal = (req,res) => {
 const deleteGoal = asyncHandler(async(req,res) => {
+    res.status(200).json({message: 'Delete Goals ${req.params.id}'})
     const goal = await Goal.findById(req.params.id)
     if(!goal) {
         res.status(400)
@@ -64,8 +58,5 @@ const deleteGoal = asyncHandler(async(req,res) => {
 
 
     res.status(200).json({id: req.params.id})
-}
 })
 
-
-module.exports = {
