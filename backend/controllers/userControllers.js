d278372357ee27da687b2272db81ee40998c9ecf
mafndi1 <191240000927@unisnu.ac.id>
2022-07-15 22:23:02 +0700

    Authenticate User

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -56,7 +56,25 @@ const registerUser = asyncHandler(async(req,res) => {
 // @access  Public
 const loginUser = (req,res) => {
 const loginUser = asyncHandler(async(req,res) => {
-    res.json({message: 'Login User'})
+    const {email, password} = req.body
+
+    // Check for user email
+    const user = await User.findOne({email})
+
+    if(user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+
+        })
+
+    } else {
+        res.status(400)
+        throw new Error('Invalid credentials')
+    }
+
 }
 })
 
