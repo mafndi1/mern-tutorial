67de3f2cc7664afeb40821a40f708fc92c99a7cf
mafndi1 <191240000927@unisnu.ac.id>
2022-07-15 22:20:03 +0700

    Hash Password

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -24,9 +24,32 @@ const registerUser = asyncHandler(async(req,res) => {
         }
 
 
-    res.json({message: 'Register User'})
-}
+        //hash password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        //create user
+        const user = await User.create({
+            name, 
+            email,
+            password: hashedPassword
+        })
+
+        if(user){
+            res.status(201).json({
+                __id: user.id,
+                name: user.name,
+                email: user.email,
+            })
+
+        } else {
+            res.status(400)
+            throw Error('Invalid user data')
+        }
 })
+
+// @desc    Login user
+}
 
 // @desc    Login user
 // @route   POST /api/users
